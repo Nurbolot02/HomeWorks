41184bc6e9e3a8639772e2ebeebac8b85f797dad
Nurbolot <ngulamidinov@gmail.com>
2022-03-07 01:40:22 +0600

    add Test Fibonacci

--- Test/Fibonacci/Fibonacci/Fibonacci/FibonacciTest.cs
+++ Test/Fibonacci/Fibonacci/Fibonacci/FibonacciTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Fibonacci.Fibonacci;
 
 namespace FibonacciTest
 {
@@ -10,7 +11,11 @@ namespace FibonacciTest
     {
         static void Main(string[] args)
         {
-
+            for (int i = 1; i < 20; i++)
+            {
+                Console.WriteLine(fibonacci(i));
+                Console.ReadKey();
+            }
         }
     }
 }
