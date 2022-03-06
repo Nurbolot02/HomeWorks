1e354f67a5ddee9baaedcd39a9f2f335f30dc589
Nurbolot <ngulamidinov@gmail.com>
2022-03-06 09:33:09 +0600

    add Lesson3 Task8 - takes a number (N) as input and produces a table of cubes of numbers from 1 to N

---
+++ Lesson3/Task8/Task8/Task8.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8
+{
+    internal class Task8
+    {
+        static void Main(string[] args)
+        {
+            /* Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+            3 -> 1, 4, 9.
+            5 -> 1, 8, 27, 64, 125
+            3 -> 1, 8, 27
+            5 -> 1, 8, 27, 64, 125 */
+
+            System.Console.Write("Enter the number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                System.Console.Write(i + " ");
+            }
+            System.Console.WriteLine();
+            for (int i = 1; i <= number; i++)
+            {
+                System.Console.Write((i * i * i) + " ");
+            }
+
+        }
+    }
+}
