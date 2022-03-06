8b72ac5de40dfa49a106142e4e7fab037ddc637e
Nurbolot <ngulamidinov@gmail.com>
2022-03-06 08:14:53 +0600

    add Lesson2 Task5 - which takes as input a digit indicating the day of the week and checks if this day is a weekend

---
+++ Lesson2/Task5/Task5/Task5.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    internal class Task5
+    {
+        static void Main(string[] args)
+        {
+            /* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+            6 -> да
+            7 -> да
+            1 -> нет */
+
+            Console.Write("Write day:");
+            int day = Convert.ToInt32(Console.ReadLine());
+
+            if (day > 0 && day <= 7)
+            {
+                if (day < 6)
+                {
+                    System.Console.WriteLine("No");
+                }
+                else
+                {
+                    System.Console.WriteLine("Yes");
+                }
+
+            }
+            else
+            {
+                System.Console.WriteLine("There is no such day of the week!");
+            }
+        }
+    }
+}
