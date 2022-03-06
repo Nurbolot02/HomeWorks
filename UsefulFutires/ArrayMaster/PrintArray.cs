3effaefbae5f54b1f59d47a5a826dae6cda9474a
Nurbolot <ngulamidinov@gmail.com>
2022-03-06 07:30:00 +0600

    Add project files.

---
+++ UsefulFutires/ArrayMaster/PrintArray.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayMaster
+{
+    public class PrintArray
+    {
+        static public void printArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+    }
+}
