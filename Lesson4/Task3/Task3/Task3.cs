c5e2328f7173beef8db839aceb8033e9922d1a38
Nurbolot <ngulamidinov@gmail.com>
2022-03-06 09:46:06 +0600

    add Lesson4 Task3 - FIND products of numbers from 1 to n

---
+++ Lesson4/Task3/Task3/Task3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Task3
+    {
+        static void Main(string[] args)
+        {
+            // Написать программу вычисления произведения чисел от 1 до n
+            Console.Write("Enter the number: ");
+            int number = int.Parse(Console.ReadLine() ?? string.Empty);
+            for (int i = 1; i <= number; i++)
+            {
+                Console.Write($"{i * i} ");
+            }
+        }
+    }
+}
