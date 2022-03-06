b8b576d685757b14de2063eb4603dff70422d573
Nurbolot <ngulamidinov@gmail.com>
2022-03-06 09:42:31 +0600

    add Lesson4 Task1 - Find the sum of numbers from 1 to a

---
+++ Lesson4/Task1/Task1/Task1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Task1
+    {
+        static void Main(string[] args)
+        {
+            // Найти сумму чисел от 1 до а 
+
+            Console.Write("Enter the number: ");
+            int number = int.Parse(Console.ReadLine() ?? string.Empty);
+            int result = findSumOf1ToN(number);
+            Console.WriteLine($"Sum of 1 to {number} = {result}");
+
+            int findSumOf1ToN(int number)
+            {
+                return number * (number + 1) / 2;
+            }
+        }
+    }
+}
