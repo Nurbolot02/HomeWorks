f4baa2e5a22087afd76ecc0c80defcea30addeb0
Nurbolot <ngulamidinov@gmail.com>
2022-03-06 22:12:12 +0600

    add Lesson4 Task5.1 Test factrorial

---
+++ Lesson4/Task5.1/Task5.1/TestFactorial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Factorial.Factorial;
+
+namespace Task5._1
+{
+    internal class TestFactorial
+    {
+        static void Main(string[] args)
+        {
+            int num = 6;
+            int result = getFactorial(num);
+            Console.WriteLine(result);
+        }
+    }
+}
