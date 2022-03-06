0e4fd9cc1ee568bd8bcb0ff2dbfe094ec969ab16
Nurbolot <ngulamidinov@gmail.com>
2022-03-06 22:10:04 +0600

    add UsefulFutires Factorial

---
+++ UsefulFutires/Factorial/Factorial/Factorial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    public class Factorial
+    {
+        public static int getFactorial(int number)
+        {
+            if (number == 0)
+
+            {
+                return 1;
+            }
+            return number * getFactorial(number - 1);
+        }
+    }
+}
