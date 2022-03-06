8d5d88815d0ce2a80e7e30da7fba8f164e9565c6
Nurbolot <ngulamidinov@gmail.com>
2022-03-07 01:24:33 +0600

    add UsefulFurires Fibonacci

---
+++ UsefulFutires/Fibonacci/Fibonacci/Fibonacci/Fibonacci/Fibonacci.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    public class Fibonacci
+    {
+        static public int fibonacci(int value)
+        {
+            if (value == 1 || value == 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return fibonacci(value - 1) + fibonacci(value - 2);
+            }
+            return value;
+        }
+    }
+}
