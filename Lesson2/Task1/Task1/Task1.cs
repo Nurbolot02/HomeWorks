b1b215f9fd6fe03229afea769bb7b09c20ac9058
Nurbolot <ngulamidinov@gmail.com>
2022-03-06 08:07:15 +0600

    add Lesson2 Task1 - shows 2-number of 3-digit number

---
+++ Lesson2/Task1/Task1/Task1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Task1
+    {
+        static void Main(string[] args)
+        {
+            /* Напишите программу, которая принимает на вход трёхзначное 
+            число и на выходе показывает вторую цифру этого числа. */
+
+            System.Console.Write("Enter 3 digit number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            number = getTwoDigit(number);
+            System.Console.WriteLine(number);
+            int getTwoDigit(int number)
+            {
+                number %= 100;
+                return number / 10;
+            }
+        }
+    }
+}
