fec06b78736a7cb4aebd2af9f841370a3c93a824
Nurbolot <ngulamidinov@gmail.com>
2022-03-06 09:22:55 +0600

    add Lesson3 Task4 - Show table  squares of numbers from 1 to N

---
+++ Lesson3/Task4/Task4/Task4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class Task4
+    {
+        static void Main(string[] args)
+        {
+            /* Задача 23: Показать таблицу 
+            квадратов чисел от 1 до N */
+            System.Console.Write("Enter the number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                System.Console.Write(i + " " + (i * i));
+                System.Console.WriteLine();
+            }
+        }
+    }
+}
