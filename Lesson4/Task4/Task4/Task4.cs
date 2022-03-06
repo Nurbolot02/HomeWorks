635564313cc6057496eb3a160cef6734f5f0fd42
Nurbolot <ngulamidinov@gmail.com>
2022-03-06 09:47:39 +0600

    add Lesson4 Task4 - Set an array of 8 elements and display them on the screen

---
+++ Lesson4/Task4/Task4/Task4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class Task4
+    {
+        static void Main(string[] args)
+        {
+            // Задать массив из 8 элементов и вывести их на экран
+            int[] numbers = new int[8];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = new Random().Next(100);
+            }
+            PrintArray(numbers);
+
+            void PrintArray(int[] numbers)
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    System.Console.Write(numbers[i] + " ");
+                }
+            }
+        }
+    }
+}
