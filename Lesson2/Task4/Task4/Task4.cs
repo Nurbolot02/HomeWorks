b8cd13e5d6c3f6b0165e018ee7a99ba7f5e6e14a
Nurbolot <ngulamidinov@gmail.com>
2022-03-06 08:13:29 +0600

    add Lesson2 Task4 - displays the third digit of the given number, or reports that there is no third digit

---
+++ Lesson2/Task4/Task4/Task4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class Task4
+    {
+        static void Main(string[] args)
+        {
+            /* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+            645 -> 5
+            78 -> третьей цифры нет
+            32679 -> 6 */
+
+            System.Console.Write("Enter the number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number > 99)
+            {
+                Console.WriteLine(number.ToString()[2]);
+            }
+            else if (number < -99)
+            {
+                Console.WriteLine(number.ToString()[3]);
+            }
+            else
+            {
+                System.Console.WriteLine("No third digit");
+            }
+        }
+    }
+}
