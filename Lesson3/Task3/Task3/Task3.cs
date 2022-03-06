8bf8994c800b10f6b5da9d85cb87251ad5751e35
Nurbolot <ngulamidinov@gmail.com>
2022-03-06 09:18:09 +0600

    add Lesson3 Task3 - Task Check Polindrom add UsefulFutires  - Check Polindrom add UsefulFutires  -  Get Number Length

---
+++ Lesson3/Task3/Task3/Task3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static GetNumberLength.GetNumberLength;
+using static CheckPolindrom.CheckPolindrom;
+
+namespace Task3
+{
+    internal class Task3
+    {
+        static void Main(string[] args)
+        {
+            /* Задача 21: Программа проверяет 
+            число на палиндромом.
+            Палиндром - слово или фраза (или число в нашем случае), 
+            которые одинаково читаются слева направо и справа 
+            налево.
+            Примеры: 545, 67876, 3443 */
+
+            System.Console.Write("Enter the number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int length = getNumberLength(number);
+            
+            checkPolindrom(number, length);
+        }
+    }
+}
---
+++ UsefulFutires/CheckPolindrom/CheckPolindrom.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckPolindrom
+{
+    static public class CheckPolindrom
+    {
+        static public void checkPolindrom(int number, int length)
+        {
+            int result = 0;
+            for (int i = 0; i < length; i++)
+            {
+                if (number.ToString()[i] == number.ToString()[length - i - 1] && number > 0)
+                {
+                    System.Console.Write((i + 1) + "-number " + number.ToString()[i] + " == " + number.ToString()[length - i - 1] + " " + (length - i) + "-number");
+                    System.Console.WriteLine();
+                    result++;
+                }
+                else if (number.ToString()[i+1] == number.ToString()[length - i]&& number < 0)
+                {
+                    System.Console.Write((i + 1) + "-number " + number.ToString()[i + 1] + " == " + number.ToString()[length - i] + " " + (length - i) + "-number");
+                    System.Console.WriteLine();
+                    result++;
+                }
+                else
+                {
+                    System.Console.WriteLine("False!");
+                    break;
+                }
+            }
+            if (result == length)
+            {
+                System.Console.WriteLine($"{number} is polindrom!");
+            }
+        }
+    }
+}
