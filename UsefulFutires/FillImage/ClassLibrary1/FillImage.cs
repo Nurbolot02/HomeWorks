107ab0b3c13f52c85a1d4d57801edfd3dbe453b0
Nurbolot <ngulamidinov@gmail.com>
2022-03-07 01:03:33 +0600

    add Test WriteFillArray add UsefulFutires FillImage add UsefulFutires PrintArray2D

---
+++ UsefulFutires/FillImage/ClassLibrary1/FillImage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PrintArray2D.PrintArray2D;
+
+namespace FillImage
+{
+    public class FillImage
+    {
+        static public int[,] fillImage(int[,] pic, int row, int col)
+        {
+            if(pic[row, col] == 0)
+            {
+                pic[row, col] = 1;
+                fillImage(pic, row - 1, col);
+                fillImage(pic, row, col - 1);
+                fillImage(pic, row + 1, col);   
+                fillImage(pic, row, col + 1);   
+            }
+            return pic;
+        }
+    }
+}
---
+++ UsefulFutires/PrintArray2D/ClassLibrary1/PrintArray2D.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintArray2D
+{
+    public class PrintArray2D
+    {
+        static public void printArray2D(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    System.Console.Write(array[i, j] + " ");
+                }
+                System.Console.WriteLine();
+            }
+        }
+        static public void printArray2D(int[,] array, int value)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if(array[i, j] == value)
+                    {
+                        System.Console.Write(" ");
+                    }
+                    else
+                    {
+                        System.Console.Write("+");
+                    }
+                }
+                System.Console.WriteLine();
+            }
+        }
+
+    }
+}
