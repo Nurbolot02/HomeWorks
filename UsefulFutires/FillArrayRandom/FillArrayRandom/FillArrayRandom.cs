8c3ff9a5d98c0cf900f6e6ee4828ed6b11a83083
Nurbolot <ngulamidinov@gmail.com>
2022-03-08 03:20:25 +0600

    add UsefulFutires  FillArrayRandom

---
+++ UsefulFutires/FillArrayRandom/FillArrayRandom/FillArrayRandom.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FillArrayRandom
+{
+    public class FillArrayRandom
+    {
+        static public int[] fillArrayRandom(int[] array)
+        {
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(100);
+            }
+            return array;
+        }
+
+        static public int[] fillArrayRandom(int[] array, int value)
+        {
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(value);
+            }
+            return array;
+        }
+
+        static public int[] fillArrayRandom(int[] array, int minValue, int maxValue)
+        {
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(minValue, maxValue);
+            }
+            return array;
+        }
+    }
+}
