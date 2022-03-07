9fde23182a95a4ea8c26277bddf953820edb1c19
Nurbolot <ngulamidinov@gmail.com>
2022-03-08 03:32:15 +0600

    add Lesson5 Task1 - Set an array of 12 elements filled numbers from [0,9]. Find the amount positive/negative array elements. Determine if the given array, some number.

---
+++ Lesson5/Task1/Task1/Task1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ArrayMaster.PrintArray;
+using static System.Console;
+using static FillArrayRandom.FillArrayRandom;
+
+namespace Task1
+{
+    public class Task1
+    {
+        static void Main(string[] args)
+        {
+            /* Задача 33: Задать массив из 12 элементов, заполненных  числами из [0,9]. 
+             * Найти сумму положительных/отрицательных элементов массива. */
+
+            int[] array = new int[12];
+            int negativeNumber = 0, number = 0;
+            array = fillArrayRandom(array, -10, 10);
+            printArray(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 0)
+                {
+                    number += array[i];
+                }
+                else
+                {
+                    negativeNumber += array[i];
+                }
+            }
+            Console.WriteLine($"negative numbers sum: {negativeNumber} \nnumbers sum : {number}");
+
+            //Задача 35: Определить, присутствует ли в заданном массиве, некоторое число 
+
+            Console.Write("Enter the number: ");
+            number = int.Parse(Console.ReadLine());
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i == array.Length - 1)
+                {
+                    Console.WriteLine(false);
+                }
+
+                else if (array[i] == number)
+                {
+                    Console.WriteLine(true);
+                    break;
+                }
+
+            }
+            ReadKey();
+        }
+    }
+}
