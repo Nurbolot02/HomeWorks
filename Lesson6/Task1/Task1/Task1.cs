80a0a0d71df79a2c0e4d7afc4f93e2bb2e852932
Nurbolot <ngulamidinov@gmail.com>
2022-03-10 22:47:25 +0600

    add Lesson6  Task1 - Find out whether three numbers are sums triangle

---
+++ Lesson6/Task1/Task1/Task1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Task1
+{
+    internal class Task1
+    {
+        static void Main(string[] args)
+        {
+            /* Задача 41: Выяснить являются ли три числа сторонами
+            треугольника.
+            Теорема о неравенстве треугольника: каждая сторона треугольника
+            меньше суммы двух других сторон.*/
+            Console.WriteLine("Задача 41: Выяснить являются ли три числа сторонами треугольника.");
+            System.Console.Write("Введите сторону A: ");
+            double a = int.Parse(Console.ReadLine());
+            System.Console.Write("Введите сторону B: ");
+            double b = int.Parse(Console.ReadLine());
+            System.Console.Write("Введите сторону C: ");
+            double c = int.Parse(Console.ReadLine());
+            bool result = IsTriangleSides(a, b, c);   
+            Console.WriteLine(result);
+            Console.WriteLine("Нажмите любую кнопку чтобы завершить программу! ");
+            Console.ReadKey();
+        }
+
+        static bool IsTriangleSides (double A, double B, double C)
+        {
+            return A < C + B || C < A + B || B < A + C;
+        }
+    }
+}
