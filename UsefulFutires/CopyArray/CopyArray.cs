83e3eb27eec17b2dd55702701beddb5d12399616
Nurbolot <ngulamidinov@gmail.com>
2022-03-11 00:14:03 +0600

    add CopyArray to UsefulFutires

---
+++ UsefulFutires/CopyArray/CopyArray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyArray
+{
+    public class CopyArray
+    {
+        static public int[] copyArray(int[] array)
+        {
+            int[] result = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                result[i] = array[i];   
+            }
+            return result;
+        }
+    }
+}
