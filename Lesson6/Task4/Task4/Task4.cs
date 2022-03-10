c72487385d1ff064d9b6a9fa39eca673618da332
Nurbolot <ngulamidinov@gmail.com>
2022-03-11 00:17:20 +0600

    add Lesson6 Task4 - CopyArray

---
+++ Lesson6/Task4/Task4/Task4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ArrayMaster.PrintArray;
+using static CopyArray.CopyArray;
+
+namespace Task4
+{
+    internal class Task4
+    {
+        static void Main(string[] args)
+        {
+            /* Задача 47: Написать программу копирования массива */
+
+            int[] numbers = { 1, 2, 3, 9, 0 };
+            int[] copyNumbers = copyArray(numbers);
+            printArray(copyNumbers);
+        }
+    }
+}
