06c005e2e130468e6a781451fa91a50c3a40354c
Nurbolot <ngulamidinov@gmail.com>
2022-03-11 14:11:35 +0600

    add Lesson6 Task5 - how many numbers greater than 0 are entered from the keyboard

---
+++ Lesson6/Task5/Task5/Task5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    internal class Task5
+    {
+        static void Main(string[] args)
+        {
+            /* Задача 42: Определить, сколько чисел больше 0 введено с клавиатуры. */
+
+            Console.Write("Enter numbers separated by spaces: ");
+            string numberString = Console.ReadLine();
+            string[] numbers = numberString.Split(' ');
+            int[] numbersInt = new int[numbers.Length];
+            int count = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbersInt[i] = int.Parse(numbers[i]);
+                if(numbersInt[i] > 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"Количество чисел больше  нуля: {count}");
+
+
+        }
+    }
+}
