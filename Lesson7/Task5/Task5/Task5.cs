11a4bc189789af9baaf685d4030cc4681379f561
Nurbolot <ngulamidinov@gmail.com>
2022-03-12 03:28:58 +0600

    add Lesson7 Task5 - Show a two-dimensional array of size m×n, filled with real numbers.

---
+++ Lesson7/Task5/Task5/Task5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PrintArray2D.PrintArray2D;
+using static FillArrayRandom.FillArrayRandom;
+
+namespace Task5
+{
+    internal class Task5
+    {
+        static void Main(string[] args)
+        {
+            /* Задача 49: Показать двумерный массив размером m×n, заполненный вещественными числами.*/
+            int m = 3, n = 3;
+            double[,] array = new double[m, n];
+            fillArrayRandom(array);
+            printArray2D(array);
+            Console.WriteLine();
+            Console.Write("press any key to close program:");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
