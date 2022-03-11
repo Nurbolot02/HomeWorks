93f32304950976decbe889805db02e15613a6b99
Nurbolot <ngulamidinov@gmail.com>
2022-03-12 00:01:54 +0600

    added support for 2D array on FillArrayRandom

--- UsefulFutires/FillArrayRandom/FillArrayRandom/FillArrayRandom.cs
+++ UsefulFutires/FillArrayRandom/FillArrayRandom/FillArrayRandom.cs
@@ -18,6 +18,7 @@ namespace FillArrayRandom
             return array;
         }
 
+
         static public int[] fillArrayRandom(int[] array, int value)
         {
             Random random = new Random();
@@ -34,6 +35,45 @@ namespace FillArrayRandom
             for (int i = 0; i < array.Length; i++)
             {
                 array[i] = random.Next(minValue, maxValue);
+            }
+            return array;
+        }
+
+        static public int[,] fillArrayRandom(int[,] array)
+        {
+            Random random = new Random();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(100);
+                }
+            }
+            return array;
+        }
+
+        static public int[,] fillArrayRandom(int[,] array, int value)
+        {
+            Random random = new Random();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(value);
+                }
+            }
+            return array;
+        }
+
+        static public int[,] fillArrayRandom(int[,] array, int minValue, int maxValue)
+        {
+            Random random = new Random();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(minValue, maxValue);
+                }
             }
             return array;
         }
