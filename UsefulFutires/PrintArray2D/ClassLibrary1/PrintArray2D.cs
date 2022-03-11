fa8e95f9b6b794206895f06a713d8e88376859e1
Nurbolot <ngulamidinov@gmail.com>
2022-03-12 03:02:47 +0600

    update PrintArray2D to UsefulFutires

--- UsefulFutires/PrintArray2D/ClassLibrary1/PrintArray2D.cs
+++ UsefulFutires/PrintArray2D/ClassLibrary1/PrintArray2D.cs
@@ -38,5 +38,17 @@ namespace PrintArray2D
             }
         }
 
+        static public void printArray2D(double[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    System.Console.Write(array[i, j] + "\t");
+                }
+                System.Console.WriteLine();
+            }
+        }
+
     }
 }
