a4cebf809830974946e854aa4a7c8b1b7d803241
Nurbolot <ngulamidinov@gmail.com>
2022-03-12 03:33:31 +0600

    add Lesson7 Task6 - Define a two-dimensional array with the following rule: Aₘₙ = m+n

---
+++ Lesson7/Task6/Task6/Task6.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PrintArray2D.PrintArray2D;
+
+namespace Task6
+{
+    internal class Task6
+    {
+        static void Main(string[] args)
+        {
+            /* Задача 51: Задать двумерный массив следующим правилом: Aₘₙ = m+n.*/
+            Console.WriteLine("Задача 51: Задать двумерный массив следующим правилом: Aₘₙ = m+n.");
+            int m = 5, n = 5;
+            int[,] array2 = new int[m, n];
+            for (int i = 0; i < array2.GetLength(0); i++)
+            {
+                for (int j = 0; j < array2.GetLength(1); j++)
+                {
+                    array2[i, j] = i + j;
+                }
+            }
+            printArray2D(array2);
+
+            Console.WriteLine();
+            Console.Write("press any key to close program:");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
