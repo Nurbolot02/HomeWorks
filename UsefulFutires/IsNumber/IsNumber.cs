c1e429b0172c3099a2a367304de46b7ed47acb07
Nurbolot <ngulamidinov@gmail.com>
2022-03-12 03:03:37 +0600

    add IsNumber to UsefulFutires

---
+++ UsefulFutires/IsNumber/IsNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsNumber
+{
+    public class IsNumber
+    {
+        static public bool isNumber(string stringNumber)
+        {
+            if (stringNumber == null || stringNumber == "") { return false; }
+            bool isNumber = true;
+            foreach (char c in stringNumber)
+            {
+                if (!char.IsDigit(c)) 
+                {
+                    isNumber = false;
+                    break; 
+                }
+                else
+                    isNumber = true;
+            }
+            return isNumber;
+        }
+    }
+}
