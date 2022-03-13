90d9466d315627ad5ca7cdb63187b633aed47268
Nurbolot <ngulamidinov@gmail.com>
2022-03-13 07:11:46 +0600

    add UsefuFutires - IgnoreElement

---
+++ UsefulFutires/IgnoreElement/IgnoreElement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IgnoreElement
+{
+    public class IgnoreElement
+    {
+        static public bool ignoreElement(int[,] array, int element, int counter)
+        {
+            bool result = false;
+            for (int i = 0; i < counter; i++)
+            {
+                if (array[i, 0] == element)
+                {
+                    array[i, 1] += 1;
+                    return true;
+                }
+            }
+            return result;
+        }
+    }
+}
