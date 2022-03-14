fe23100ba5bdaf2639b47e36810674f33304951c
Nurbolot <ngulamidinov@gmail.com>
2022-03-14 23:12:28 +0600

    update UsefulFutires FillArrayRandom - addded support 3-dimensional array

--- UsefulFutires/FillArrayRandom/FillArrayRandom/FillArrayRandom.cs
+++ UsefulFutires/FillArrayRandom/FillArrayRandom/FillArrayRandom.cs
@@ -90,5 +90,52 @@ namespace FillArrayRandom
             }
             return array;
         }
+        static public int[,,] fillArrayRandom(int[,,] array)
+        {
+            Random random = new Random();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    for(int k = 0; k < array.GetLength(2); k++)
+                    {
+                        array[i, j, k] = random.Next(100);
+                    }
+                }
+            }
+            return array;
+        }
+
+        static public int[,,] fillArrayRandom(int[,,] array, int value)
+        {
+            Random random = new Random();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    for (int k = 0; k <= array.GetLength(2); k++)
+                    {
+                        array[i, j, k] = random.Next(value);
+                    }
+                }
+            }
+            return array;
+        }
+
+        static public int[,,] fillArrayRandom(int[,,] array, int minValue, int maxValue)
+        {
+            Random random = new Random();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    for (int k = 0; k < array.GetLength(2); k++)
+                    {
+                        array[i, j, k] = random.Next(minValue, maxValue);
+                    }
+                }
+            }
+            return array;
+        }
     }
 }
