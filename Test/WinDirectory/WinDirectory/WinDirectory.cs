80bb988b7b4d5254f69941e5fe0b8bee8817a79c
Nurbolot <ngulamidinov@gmail.com>
2022-03-15 22:57:58 +0600

    add Test -  WinDirectory

---
+++ Test/WinDirectory/WinDirectory/WinDirectory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinDirectory
+{
+    internal class WinDirectory
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\GBEducation", indent = " ";
+            CatalogInfo(path,indent);
+
+
+        }
+
+         static void CatalogInfo (string path, string indent = "")
+        {
+            DirectoryInfo catalog = new DirectoryInfo(path);   
+            DirectoryInfo[] dirs = catalog.GetDirectories();
+            for (int i = 0; i < dirs.Length; i++)
+            {
+                Console.WriteLine($"{indent}{dirs[i].Name}");
+                CatalogInfo(dirs[i].FullName, indent + " ");
+            }
+
+            FileInfo[] files = catalog.GetFiles();
+            for(int i = 0; i < files.Length; i++)
+            {
+                Console.WriteLine($"{indent}{files[i].Name}");
+            }
+        }
+    }
+}
