5687db10198a7c140453d557185669a01437913b
Nurbolot <ngulamidinov@gmail.com>
2022-03-16 07:57:21 +0600

    add Lesson9 Task1 - print numbers from 1 to n using recursion RU вывести числа от 1 до N с помощью рекурсии

---
+++ Lesson9/Task1/Task1/Task1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static IsNumber.IsNumber;
+
+namespace Task1
+{
+    internal class Task1
+    {
+        static void Main(string[] args)
+        {
+            /* Задача 66: Задайте значение N. Напишите программу, которая выведет
+            все натуральные числа в промежутке от 1 до N.
+            N = 5 -> "1, 2, 3, 4, 5"
+            N = 6 -> "1, 2, 3, 4, 5, 6"
+             */
+            Console.WriteLine("Задача 66: Задайте значение N. Напишите программу, которая выведет"+
+            "все натуральные числа в промежутке от 1 до N."+
+            "N = 5-> 1, 2, 3, 4, 5"+
+            "N = 6-> 1, 2, 3, 4, 5, 6");
+            string rowString = "enter the number  n: ";
+            int n = isNumber(rowString, true);
+            ShowNumberN(n);
+
+        }
+
+        static void ShowNumberN(int n)
+        {
+            if(n < 1)
+            {
+                ShowNumberN(n);
+            }
+            else if(n > 1)
+            {
+                ShowNumberN(n - 1);
+            }
+            Console.Write(n + " ");
+        }
+    }
+}
