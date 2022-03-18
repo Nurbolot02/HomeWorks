c478a6938f559f4796f988a06fc214c46f8c6b75
Nurbolot <ngulamidinov@gmail.com>
2022-03-18 12:54:50 +0600

    add Lesson10 Task2 - Show Words ^ n degree RU:  В некотором машинном алфавите имеются четыре буквы «а», «и», «с»  и «в». Покажите все слова, состоящие из n букв, которые можно построить избукв этого алфавита.

---
+++ Lesson10/Task1/Task1/Task1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static IsNumber.IsNumber;
+
+namespace Task1
+{
+    public class Task1
+    {
+        static void Main(string[] args)
+        {
+            /* Задача 70: Напишите программу, которая на вход принимает два числа и выдаёт
+            первые N чисел, для которых каждое следующее равно сумме двух предыдущих.
+            3 и 4, N = 5 -> 3 4 7 11 18
+            6 и 10, N = 4 -> 6 10 16 26 */
+            Console.WriteLine("Задача 70: Напишите программу, которая на вход принимает два числа и выдаёт" +
+                "первые N чисел, для которых каждое следующее равно сумме двух предыдущих." +
+                "3 и 4, N = 5 -> 3 4 7 11 18" +
+                "6 и 10, N = 4 -> 6 10 16 26" +
+                "");
+            string rowString = "enter the 1-number: ";
+            int n1 = isNumber(rowString, true);
+            string columnsString = "enter the 2-number: ";
+            int n2 = isNumber(columnsString, true);
+            string str = "enter the n: ";
+            int n = isNumber(str, true);
+
+
+            ShowNumber(n1, n2, n);
+
+        }
+
+        static void ShowNumber( int n1, int n2, int n)
+        {
+            if(n >= 1)
+            {
+                Console.Write(n1 + " ");
+                ShowNumber(n2, n2 + n1, n - 1);
+            }
+        }
+    }
+}
---
+++ Lesson10/Task2/Task2/Task2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static IsNumber.IsNumber;
+
+namespace Task2
+{
+    internal class Task2
+    {
+        static void Main(string[] args)
+        {
+            /* Задача 71: В некотором машинном алфавите имеются четыре буквы «а», «и», «с»
+            и «в». Покажите все слова, состоящие из n букв, которые можно построить из
+            букв этого алфавита.
+            n = 2 -> аа, ии, сс, вв, аи, иа, ис, си, ас, са,
+            ав, ва, ви, ив, св, вс */
+
+            string text = "аисв";
+            string rowString = "enter the n: ";
+            int num = isNumber(rowString, true);
+
+            string[] array = Alfavit(text, num);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+        static string[] Alfavit(string text, int n)
+        {
+            if (n == 0) return new string[] { "" };
+            string[] mass = Alfavit(text, n - 1);
+            string[] result = new string[text.Length * mass.Length];
+            int k = 0;
+            for (int i = 0; i < mass.Length; i++)
+            {
+                for (int j = 0; j < text.Length; j++)
+                {
+                    result[k] = mass[i] + text[j];
+                    k++;
+                }
+            }
+            return result;
+        }
+    }
+}
