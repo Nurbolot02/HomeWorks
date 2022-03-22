d74290c07a1a789a51d114e194a6039eec36b9fd
Nurbolot <ngulamidinov@gmail.com>
2022-03-22 13:13:21 +0600

    add Lesson4 Task7  -  takes a number as input and returns the sum of the digits in the number RU: принимает на вход число и выдаёт сумму цифр в числе

---
+++ Lesson4/Task7/Task7/Task7.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static IsNumber.IsNumber;
+
+namespace Task7
+{
+    internal class Task7
+    {
+        static void Main(string[] args)
+        {
+            /* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+                452 -> 11
+                82 -> 10
+                9012 -> 12 */
+            string numString = "enter the number: ";
+            int n = isNumber(numString, true), result = 0;
+
+
+            while(n > 0)
+            {
+                result += n % 10;
+                n /= 10;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
