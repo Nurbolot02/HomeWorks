d8426a87b518a51961b1dc73f1ce11a69aa38432
Nurbolot <ngulamidinov@gmail.com>
2022-03-22 13:03:07 +0600

    add UsefulFutires Exponentation A to B

---
+++ UsefulFutires/Exponentiation/Exponentiation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exponentiation
+{
+    public class Exponentiation
+    {
+        static public int exponentiation(int a, int b)
+        {
+            if (b == 1)
+            {
+                return a;
+            }
+            return a * exponentiation(a, b - 1); ;
+        }
+    }
+}
