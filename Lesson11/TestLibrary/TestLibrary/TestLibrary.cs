13814a8a9baa0b79f5fb0c10917a521cc071e2e0
Nurbolot <ngulamidinov@gmail.com>
2022-04-14 19:17:15 +0600

    add Lesson11 - TestLibrary ArrayMaster and ArrayMaster2D

---
+++ Lesson11/TestLibrary/TestLibrary/TestLibrary.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ListMaster;
+
+namespace TestLibrary
+{
+    internal class TestLibrary
+    {
+        static void Main(string[] args)
+        {
+            bool printType = true;
+            ArrayMaster createdArray = new ArrayMaster();
+            createdArray.SetSize(10);
+            createdArray.FillRandom(10, 50);
+            createdArray.Print(printType);
+            createdArray.SortBuble();
+            createdArray.Print(printType);
+            Console.WriteLine();
+
+            ArrayMaster2D array2D = new ArrayMaster2D();
+            array2D.SetSize(3,4);
+            array2D.Print();
+            array2D.FillRandom(50, 85);
+            array2D.Print();
+
+        }
+    }
+}
